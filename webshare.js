4003eb059dc0a2d39eca9b2d5318fee05f6ee6d4
Alper Ergüne <alperergune8@outlook.com>
2023-09-13 14:58:57 +0300

    fixed graph config not showing, tidied code

--- webshare.js
+++ webshare.js
@@ -14,4 +14,9 @@ function shareContent() {
       console.error("Web Share API not supported.");
     }
   }
-  
+if (!navigator.share) {
+  document.getElementById("appleShareButton").style.display = "none"; // Hide the apple share button if the Apple Web Share API is not supported
+}
+else{
+  document.getElementById("linkShareButton").style.display = "none"; // Hide the link share button if the API is supported
+}
