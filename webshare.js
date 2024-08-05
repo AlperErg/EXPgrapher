02e436e556f36b9c7e41e96e2d184a87114b16aa
Alper Ergüne <alperergune8@outlook.com>
2024-08-05 19:12:49 +0300

    Link sharing without Web Share API

--- webshare.js
+++ webshare.js
@@ -3,7 +3,7 @@ function shareContent() {
     const data = {
         title: "BetterIB EXPgrapher",
         text: "Link to a graph",
-        url: graphLink, //not working at the moment maybe transfer this code to index html
+        url: graphLink, 
     };
   
     if (navigator.share) {
@@ -12,12 +12,19 @@ function shareContent() {
         .then(() => console.log("Successfully shared!"))
         .catch((error) => console.error("Error sharing:", error));
     } else {
-      console.error("Web Share API not supported.");
+      console.log("Web Share API not supported.");
+      if (navigator.clipboard) {
+          navigator.clipboard.writeText(graphLink)
+              .then(() => console.log("URL copied to clipboard!"))
+              .catch((error) => console.error("Error copying to clipboard:", error));
+      } else {
+          console.error("Clipboard API not supported.");
+      }
     }
-  }
+}
 if (!navigator.share) {
   document.getElementById("appleShareButton").style.display = "none"; // Hide the apple share button if the Apple Web Share API is not supported
 }
 else{
   document.getElementById("linkShareButton").style.display = "none"; // Hide the link share button if the API is supported
-}
+}  
